389887328e3efe76cf58958488bdaa61fedc0fae
Lmanzplusratio <119910415+Lmanzplusratio@users.noreply.github.com>
2026-08-29 05:35:51 +0000

    Made Storyline!

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
 
-// Lab 1 - Isaiah Salvatierra
-// CIS 5 Week 01
+using std::cout;
+using std::cin;
+using std::string;
 
-int main() {
-    std::cout << "Hello World!\n";
-    return 0;
-  }
+int main() {string first, last, years, vision ;
+  int Year = 0; 
+  cout << "First name: ";
+  cin >> first;
+  cout << "Last Name: ";
+  cin >> last;
+  cout << "What are yours goals for the next 5 yearss?\nLet's start with the first year: ";
+  cin >> years;
+  cout << "What about in 3 Years? : ";
+  cin >> years;
+  cout << "5 Years? Have you planned and visioned that far ahead in your life? : ";
+  cin >> vision;
+  cout << "YOU GOT THIS!!\n ";
+  return 0;
+}
